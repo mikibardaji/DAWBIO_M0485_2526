44a6207fea2cefb4ead179093e153c8b4396c31e
Miguel Angel Bardaji Bosch <mabardaji@proven.local>
2025-09-22 17:23:38 +0200

    Tots2

---
+++ A1/Variables/VariablesJava/src/AreaCuadrado.java
@@ -0,0 +1,13 @@
+public class AreaCuadrado {
+
+    /*Programa que calcula l'àrea d'un quadrat el costat del qual s'introdueix per teclat. */
+    public static void main(String[] args) {
+      //1.- 
+      
+      //2.-
+
+
+
+
+    }
+}
