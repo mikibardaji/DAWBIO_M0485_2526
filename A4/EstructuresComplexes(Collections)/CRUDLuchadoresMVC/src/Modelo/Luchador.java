3b4718624abb56469aeaa4b66cfd4f00174fe352
mabardaji <m@hotmail.com>
2026-03-12 18:17:51 +0100

    MVCLuchadorPorAcabar

---
+++ A4/EstructuresComplexes(Collections)/CRUDLuchadoresMVC/src/Modelo/Luchador.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+
+/**
+ *
+ * @author mabardaji
+ */
+public class Luchador {
+    private int idLicencia;
+    private String nombre;
+    private int peso;
+
+    public Luchador(int idLicencia, String nombre) {
+        this.idLicencia = idLicencia;
+        this.nombre = nombre;
+        this.peso = 1;  //como el setter lanza una excepcion
+        //no me interesa llamarlo porque si saltara la excepcion
+        //el objeto en el constructor no se crearia
+    }
+
+    public int getIdLicencia() {
+        return idLicencia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setIdLicencia(int idLicencia) {
+        this.idLicencia = idLicencia;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPeso(int peso) throws NotValidWeightException {
+        if (peso>0 && peso <=200)
+        {
+            this.peso = peso;
+        }
+        else
+            throw new NotValidWeightException();
+    }
+
+    @Override
+    public String toString() {
+        return  nombre +  " licencia " + idLicencia+  " con un peso de " + peso +  " kg.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Luchador)) {
+            return false;
+        }
+        final Luchador other = (Luchador) obj;
+        return this.idLicencia == other.idLicencia;
+    }
+    
+    
+}
